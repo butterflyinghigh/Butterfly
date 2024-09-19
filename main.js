a4174029c9a8f8407f7ced103d992faaaf2b74ba
butterflyinghigh <butterfly.fsociety@gmail.com>
2024-09-20 00:00:32 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+process.on('uncaughtException', console.error);
+require('./lib/other.js');
+require('utils-mf/index.js');
+const Pino = require('pino');
+const {
+   makeInMemoryStore,
+   useMultiFileAuthState,
+   DisconnectReason
+} = require('@adiwajshing/baileys');
+const {
+   signalGroup
+} = require('utils-mf');
+const startWhatsApp = async () => {
+   const store = makeInMemoryStore({
+      logger: Pino().child({
+         level: 'silent',
+         stream: 'store'
+      })
+   });
+   const { state, saveCreds } = await useMultiFileAuthState('./sessions');
+   const conn = await signalGroup(state, store);
+   conn.ev.on('connection.update', (update) => {
+      const {
+         connection,
+         lastDisconnect
+      } = update;
+      if (connection === 'open') {
+         console.log(`🟢 Online`)
+      } else if (connection === 'connecting') {
+         console.log(`🟡 Reconnecting`)
+      } else if (connection === 'close') {
+         console.log(`🔴 Disconnected`)
+         lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? startWhatsApp() : startWhatsApp();
+      }
+   });
+   store.bind(conn.ev);
+   conn.ev.on('creds.update', saveCreds);   
+};
+startWhatsApp();
